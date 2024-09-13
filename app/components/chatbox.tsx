476ef3ae0d6ace9761a43ac64d1ec82a59ddf629
0xKona <konarobinson@outlook.com>
2024-09-13 14:54:42 +0100

    remove unused import

--- app/components/chatbox.tsx
+++ app/components/chatbox.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect, sendMsg } from '../api-connections/chat';
+import { sendMsg } from '../api-connections/chat';
 import { Button, View } from 'react-native';
 
 const ChatBox = () => {
